c989f6f8cb6a17efdd1745aefabe1f2e5614d874
GrayfienHalim <grayfien2002@gmail.com>
2022-09-13 14:11:37 +0700

    Initial commit

---
+++ app/src/main/java/com/grayfien/view_binding_10603/Task.kt
@@ -0,0 +1,2 @@
+package com.grayfien.view_binding_10603
+data class Task(val chapter:String, val title:String, val details:String)
---
+++ app/src/main/java/com/grayfien/view_binding_10603/TaskList.kt
@@ -0,0 +1,11 @@
+package com.grayfien.view_binding_10603
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Melakukan kebiasaan sehari-hari"),
+        Task("Chapter 2", "Main Valorant", "Mencari RR yang hilang karena lose streak"),
+        Task("Chapter 3", "Ngopi", "Ngopi dulu karna defeat lagi"),
+        Task("Chapter 4","Stress", "Main malah lose streak lagi rank turun"),
+        Task("Chapter 5", "Message", "Valorant is a free game but consume ur sanity every time u match")
+    )
+}
